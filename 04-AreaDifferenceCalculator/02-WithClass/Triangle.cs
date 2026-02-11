1fd26cb359e06a007623fac1d6b7960b6740a2ca
Samuel de Oliveira <153336559+SamuelOliveira-tech@users.noreply.github.com>
2026-02-11 13:55:04 -0300

    Add Triangle class with public attributes for sides

---
+++ 04-AreaDifferenceCalculator/02-WithClass/Triangle.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriangleCalculatorWithClass
+{
+    internal class Triangle
+    {
+        //public para os atributos poderem ser acessados em outro arquivo
+        public double A;
+        public double B;
+        public double C;
+    }
+}
+
